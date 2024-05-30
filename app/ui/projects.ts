6d89c07a29e9e11333070ca267c7ec30039fa456
Alex <alwx162@gmail.com>
2024-05-29 23:19:02 -0500

    chat

--- app/ui/projects.ts
+++ app/ui/projects.ts
@@ -1,5 +1,13 @@
 export const projects = [
   {
+    'title': 'Chat Developers',
+    'description': 'Este proyecto muestra un chat usando Stream, el cual tiene como objetivo principal brindar un espacio para que los desarrolladores puedan comunicarse y compartir información sobre tecnologías de desarrollo.',
+    'techs': ['react', 'next','node'],
+    'img': '/img/chat-developers.png',
+    'github': 'https://github.com/Alexz162/chat-online',
+    'link': 'https://server-chat-bpgn.onrender.com/'
+  },
+  {
     'title': 'Transportes SIT',
     'description': 'Este proyecto esta inspirado en el servicio de transporte de la ciudad de Arequipa, el cual tiene como objetivo principal brindar información en tiempo real sobre las rutas y paraderos de los buses de la ciudad.',
     'techs': ['django', 'java'],
