d847b91cafb8a8b03c460f44c9b50c6519c70881
Alex <alwx162@gmail.com>
2024-06-17 17:11:40 -0500

    time

--- app/ui/trabajos.ts
+++ app/ui/trabajos.ts
@@ -2,7 +2,7 @@ export const trabajos = [
   {
     rol: 'Full Stack Developer ',
     empresa: 'Exoap',
-    descripcion: 'Gracias a Programa de prácticas estudiantiles CEAP (Alianza del Pacífico).  Diseño de arquitectura de software, desarrollo frontend y backend para plataforma e-comerce, ReactJS, Vtex, Redux, TypeScript, Jquery, Csss, GraphQl, Python, Django, base de datos PostgreSQL y control de versiones en GitHub.',
+    descripcion: 'Gracias a Programa de prácticas estudiantiles CEAP (Alianza del Pacífico).  Diseño de arquitectura de software, desarrollo frontend y backend para plataforma e-comerce, ReactJS, Vtex, Redux, TypeScript, Jquery, Css, GraphQl, Python, Django, base de datos PostgreSQL y control de versiones en GitHub.',
     flechaInicio: ' 2022',
     flechaFin: ' 2023'
   },
@@ -16,7 +16,7 @@ export const trabajos = [
   {
     rol: 'Frelance ',
     empresa: '',
-    descripcion: ' Desarrollo de aplicaciones web con ReactJS, Vtex, ReactNative, NextJS, redux, Tailwind, Google Cloud, NodeJS, expressJS. Base de datos MongoDB, Resolución de problemas y programación estructurada.',
+    descripcion: ' Desarrollo de aplicaciones web con ReactJS, Vtex, ReactNative, NextJS, redux, Tailwind, Google Cloud, NodeJS, ExpressJS. Base de datos MongoDB, Resolución de problemas y programación estructurada.',
     flechaInicio: ' 2023',
     flechaFin: ' 2024'
   },
